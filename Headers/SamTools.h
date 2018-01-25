0d8b56ebb6856ba8ec45e94c371221888d17a341
gilad@msbitsoftware.com <gilad@msbitsoftware.com>
2018-01-25 10:11:57 +0200

    initial commit

---
+++ Headers/SamTools.h
@@ -0,0 +1,19 @@
+//
+//  SamTools.h
+//  SamTools
+//
+//  Created by Roy on 20/06/2016.
+//  Copyright © 2016 sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SamTools.
+FOUNDATION_EXPORT double SamToolsVersionNumber;
+
+//! Project version string for SamTools.
+FOUNDATION_EXPORT const unsigned char SamToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SamTools/PublicHeader.h>
+
+
